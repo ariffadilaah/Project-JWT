c9956216ec25ece526aeb53a6d4e8b3eead349ec
ArifFadilah <ariffadilah16@gmail.com>
2021-06-05 15:18:40 +0700

    berhasil menambahkkan data

--- controller.js
+++ controller.js
@@ -30,3 +30,20 @@ exports.tampilberdasarkanid = function(req,res){
             }
         });
 };
+
+//menambahkan data
+exports.tambahAliansi = function(req,res){
+    var nim = req.body.nim;
+    var nama = req.body.nama;
+    var jurusan = req.body.jurusan;
+
+    connection.query('INSERT INTO aliansi (nim,nama,jurusan) VALUES(?,?,?)', 
+        [nim,nama,jurusan],
+        function (error, rows, fields){
+            if(error){
+                console.log(error);
+            }else {
+                response.ok("berhasil menambahkan data", res)
+            }
+        });
+};
